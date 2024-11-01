1857d4155cc32f048f680395673b2f4fd421911e
almustanir <olajidemubarak561@gmail.com>
2024-11-01 22:15:57 +0100

    auth done

--- backend/lib/mailtrap.js
+++ backend/lib/mailtrap.js
@@ -5,11 +5,11 @@ dotenv.config();
 
 const TOKEN = process.env.MAILTRAP_TOKEN;
 
-export const mailtrapclient = new MailtrapClient({
-    token: TOKEN,
+export const mailtrapClient = new MailtrapClient({
+	token: TOKEN,
 });
 
 export const sender = {
-    email: process.env.EMAIL_FROM,
-    name: process.env.EMAIL_FROM_NAME,
-}
+	email: process.env.EMAIL_FROM,
+	name: process.env.EMAIL_FROM_NAME,
+};
